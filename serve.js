310cea7e923ff3bdb57e91c07948546e5eb09a92
bosgood <bosgood@gmail.com>
2014-10-08 12:50:28 -0500

    Making the serve script not so terrible

--- serve.js
+++ serve.js
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 //
 // serve.coffee
 // short and sweet static asset server for a local directory
@@ -7,13 +8,16 @@
 var express = require('express');
 var path = require('path');
 var app = express();
-var port = process.env.PORT || 3000;
+console.log(process.argv);
+var port = parseInt(process.env.PORT || process.argv[2] || 3000, 10);
 app.set('port', port);
 app.use(express.bodyParser());
 app.use(express.methodOverride());
 app.use(app.router);
-app.use(express.static(path.join(__dirname, '')));
+var servePath = process.argv[3] || '';
+app.use(express.static(servePath));
 
 app.listen(port, function() {
-    console.log("static server listening on " + port);
+  console.log("static server listening on " + port);
+  console.log("serving " + __dirname + " from " + servePath);
 });
